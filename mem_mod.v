f2066011847d63fe0e96bf08f999c7e5a046d37e
Sergio J <sergisimo1@gmail.com>
2019-07-07 13:26:51 -0500

    compiled module ready for testing

--- mem_mod.v
+++ mem_mod.v
@@ -4,34 +4,58 @@
 // 
 // 
 // Route data to/from memory array as per ReadWrite bit
-// ReadWrite 1 = Read; at risingedge
-// ReadWrite 0 = Write at falling edge
-// nEnable is active low
+// ReadWrite 1 = Read from memory; at risingedge
+// ReadWrite 0 = Write to memory;  at falling edge
+//
+// nEnable is active low, this allows data to be either written 
+// or read from at the respective clock edge dependent on the ReadWrite signal
+//
+// Page used below for reference
 // http://www.asic-world.com/examples/verilog/ram_sp_sr_sw.html
-
-
 
 module memory (dataBus, address, nEnable, ReadWrite, clk);
 
+	
+// Input Ports for memory control and clk
 input	ReadWrite, nEnable, clk;
 input	[3:0] address;
+	
+// I/O tristated bus for all communication to other modules
+inout 	[255:0] dataBus; 
 
-inout 	[255:0] dataBus; // single I/O 256 bit tristated bus
+// Memory block containing 8 addresses, 256 bits deep
+reg [255:0] MemArray[0:7]; 
+	
+// Internal registers directing read data
+reg  [255:0] outArray;
 
-reg [255:0] MemArray[0:7]; // creates 8, 256 bit arrays for use as memory
-wire [255:0] inputArray;
+    
+// Tristate buffer control
+// Memory only writes to bus if nEnable is low and ReadWrite is high, prompting a read
+// When nEnable is low, connect dataBus to internal register outArray which has the 
+// data from the requested address, otherwise tristate
+assign dataBus = (!nEnable && !ReadWrite) ? outArray : 256'bz;
+	
 
-assign dataBus = (!nEnable) ? inputArray : 256'bz;
-assign inputArray = dataBus;
 
-
+// Read
+// positive edge clock read when ReadWrite is high and nEnable is low
 always @ (posedge clk)
 begin
-	if (ReadWrite)	
-		MemArray[address] = inputArray;  // performs a read
-			
-	else dataBus = MemArray[address]; // performs a write
+	if (ReadWrite && !nEnable)	
+		outArray = MemArray [address];  // performs a read
+end
+	
+// Write
+// negative edge clock read when Read Write is low and nEnable is low
+always @ (negedge clk)
+begin
+	if (!ReadWrite && !nEnable)
+		MemArray [address] = dataBus;
 end
 
-
 endmodule
+
+		
+		
+		

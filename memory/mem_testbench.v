5412b35f4125a62367dae93583d25dc5484b4089
Sergio J <sergisimo1@gmail.com>
2019-07-07 13:44:54 -0500

    Update mem_testbench.v

--- memory/mem_testbench.v
+++ memory/mem_testbench.v
@@ -1,2 +1,13 @@
 // Sergio Jimenez
 // Testbench for Memory module
+// Imports 2kb memory module created for RISC processor HDL project and 
+// tests the read and write functionality to all addresses.
+//
+// Will generate a clock with a 10 ns period, and will feed data to each 
+// memory address, then will attempt to read each memory address to confirm
+// the write, and will overwrite one address to confirm data is being stored
+// as expected
+//
+
+`timescale 1ns/1ns
+
